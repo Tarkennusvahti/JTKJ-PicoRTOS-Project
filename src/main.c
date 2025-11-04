d471a0ecd24e31f0408321467001db9276cf5610
Nestor Tiitinen <ntiitinen87@gmail.com>
2025-11-04 16:47:06 +0200

    Modified variables

--- src/main.c
+++ src/main.c
@@ -21,8 +21,8 @@ volatile enum state programState = LISTEN;
 
 
 // Globaalit muuttujat datan tallentamista varten 
-volatile float g_accel_x = 0.0, g_accel_y = 0.0, g_accel_z = 1.0;
-volatile float g_gyro_x = 0.0, g_gyro_y = 0.0, g_gyro_z = 0.0;
+volatile float pos_x = 0.0, pos_y = 0.0, pos_z = 1.0;
+volatile float accel_x = 0.0, accel_y = 0.0, accel_z = 0.0;
 
 
 // Ongelma: nappia painaessa välilyöntejä tuli useampi, duck.ai hakukoneen esimerkistä mallia
@@ -55,26 +55,20 @@ static void btn_fxn(uint gpio, uint32_t eventMask) {
 static void sensor_task(void *arg){
     (void)arg;
 
-    // Käynnistetään ICM42670-sensori oletusarvoilla
-    if (ICM42670_start_with_default_values() != 0) {
-        printf("IMU-sensorin käynnistys epäonnistui! Taski pysähtyy.\n");
-        for(;;) { vTaskDelay(pdMS_TO_TICKS(1000)); }
-    }
-
     // Muuttujat datan lukemista varten
-    float ax, ay, az, gx, gy, gz, t;
+    float px, py, pz, ax, ay, az, t;
 
     // Luetaan dataa ikuisessa loopissa
     for(;;){
-        ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t);
+        ICM42670_read_sensor_data(&px, &py, &pz, &ax, &ay, &az, &t);
 
         // Tallennetaan uusin data globaaleihin muuttujiin
-        g_accel_x = ax;
-        g_accel_y = ay;
-        g_accel_z = az;
-        g_gyro_x = gx;
-        g_gyro_y = gy;
-        g_gyro_z = gz;
+        pos_x = px;
+        pos_y = py;
+        pos_z = pz;
+        accel_x = ax;
+        accel_y = ay;
+        accel_z = az;
 
         // Kuinka monta kertaa sekunnissa dataa luetaan, atm kahdesti sekunnissa
         vTaskDelay(pdMS_TO_TICKS(500));
@@ -88,21 +82,21 @@ static void morse_task(void *arg){
     (void)arg;
 
     for(;;){
-        float ax = g_accel_x;
-        float az = g_accel_z;
+        float px = pos_x;
+        float pz = pos_z;
 
         // Tarkista oikea käännös: x ~= 1 ja z ~= 0
         if (programState == LISTEN) {
 
             // Tarkista vasen käännös: x ~= -1 ja z ~= 0
-            if (ax > -1.5 && ax < -0.5 && az < 0.5 && az > -0.5) {
+            if (px > -1.5 && px < -0.5 && pz < 0.5 && pz > -0.5) {
                 printf(".");
                 toggle_led();
                 buzzer_play_tone(4000, 100);
                 programState = WAIT_FOR_RESETTING;
 
             // Tarkista oikea käännös: x ~= 1 ja z ~= 0
-            } else if (ax < 1.5 && ax > 0.5 && az < 0.5 && az > -0.5) {
+            } else if (px < 1.5 && px > 0.5 && pz < 0.5 && pz > -0.5) {
                 printf("_");
                 toggle_led();
                 buzzer_play_tone(1000, 500);
@@ -111,7 +105,7 @@ static void morse_task(void *arg){
 
         } else if (programState == WAIT_FOR_RESETTING) {
             // Tarkista lepoasento: x ~= 0 ja z ~= 1
-            if (ax < 0.5 && ax > -0.5 && az > 0.5) {
+            if (px < 0.5 && px > -0.5 && pz > 0.5) {
                 programState = LISTEN;
                 toggle_led();
             }
@@ -122,15 +116,6 @@ static void morse_task(void *arg){
 }
 
 
-/*
-static void usbTask(void *arg) {
-    (void)arg;
-    while (1) {
-        tud_task();              
-    }
-}*/
-
-
 // AI: Claude Sonnet 4.5
 // Prompt(?): Analysoi koodi main.c ja muokkaa main funktio toimivaksi. Älä luo uutta, muokkaa olemassa olevaa.
 // Lisätty buzzerin alustus ja init_hat_sdk jonka AI poisti.
@@ -138,9 +123,11 @@ int main() {
     stdio_init_all();
     
     // Alustetaan tarvittavat asiat
-    init_hat_sdk();
-    sleep_ms(300); //Wait some time so initialization of USB and hat is done.
+    stdio_init_all();   // Tuki USB:lle
+    init_hat_sdk();     // Ledi pois ja i2c alustukset
+    sleep_ms(300);      //Wait some time so initialization of USB and hat is done.
 
+    ICM42670_start_with_default_values();
     init_buzzer();
     init_button1();
     init_button2();
